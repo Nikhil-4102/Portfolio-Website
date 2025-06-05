32760d783ba72b62c54b0bfbb59823cc5113b414
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-06-05 15:39:13 +0530

    home and about almost done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+
 import { NevbarComponent } from './components/nevbar/nevbar.component';
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactsComponent } from "./components/contacts/contacts.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink,NevbarComponent],
+  imports: [ NevbarComponent, HomeComponent, AboutComponent, ProjectsComponent, ContactsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
+        
     {
         path:'about', component:AboutComponent
     },
