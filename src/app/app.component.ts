f4cb046c4d303ab3367771ae24e2a06a33e0305f
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-07-20 02:03:44 +0530

    edu/exp component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { AboutComponent } from "./components/about/about.component";
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { ContactsComponent } from "./components/contacts/contacts.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { EduComponent } from "./components/edu/edu.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ NevbarComponent, HomeComponent, AboutComponent, ProjectsComponent, ContactsComponent, FooterComponent],
+  imports: [NevbarComponent, HomeComponent, AboutComponent, ProjectsComponent, ContactsComponent, FooterComponent, EduComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { EduComponent } from './components/edu/edu.component';
 import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
@@ -11,6 +12,9 @@ export const routes: Routes = [
     },
     {
         path:'projects', component:ProjectsComponent
+    },
+    {
+        path:'edu_exp', component:EduComponent
     },
     {
         path:'contact', component:ContactsComponent
